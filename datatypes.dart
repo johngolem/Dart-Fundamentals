8f4617d93d69465a0534bc3e7bbb4838ddd67317
John Paul <johnpaul@trees.local>
2022-04-06 07:34:08 +0300

    add data types

--- datatypes.dart
+++ datatypes.dart
@@ -11,7 +11,7 @@ main() {
 
   var amount2 = 200;
 
-  print('Amount1: $amount1 | Amount2 $amount2 \n');
+  print('\n Amount1: $amount1 | Amount2 $amount2 \n');
 
   String name1 = 'john';
   var name2 = 'Paul';
@@ -22,4 +22,17 @@ main() {
   var dmount2 = 120.23;
 
   print('Double1:$damount1 | Double2: $dmount2 \n');
+
+  bool isItTrue1 = true;
+  var isItTrue2 = false;
+
+  print('isItTrue1: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+
+  dynamic weakVariable = 100;
+  print('Weakvariable 1: $weakVariable \n');
+
+
+   weakVariable = 'Dart Programming';
+
+   print ("WeakVariable 2: $weakVariable \n");
 }
