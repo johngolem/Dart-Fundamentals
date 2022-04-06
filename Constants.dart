345c72674a130d75d702855bbf4ec79949b8ba24
John Paul <johnpaul@trees.local>
2022-04-06 09:48:36 +0300

    static const concept done

--- Constants.dart
+++ Constants.dart
@@ -3,16 +3,28 @@ main() {
   const aConstBool = true; //bool constant
   const aConstString = "kwenda"; // str constant
 
+  final double Pi = 3.1459;
+
+  print(Pi);
+
   print(aConstString);
   print(aConstBool);
   print(aConstNum);
 
-
   print(aConstString.runtimeType);
+
   /// runtimeType is an inbuilt function that checks the type created during runtime
   print(aConstBool.runtimeType);
   print(aConstNum.runtimeType);
 
+  /// once you declare a variable but dont assign a value to it, it will be regarded as null object  (empty)
+  ///
+}
 
-  /// once you declare a variable but dont assign a value to it, it will be regarded as null object  (empty)  
+class Myconst {
+  final gravity = 9.8;
+
+  static const PI = 3.142;
+
+  /// in class level, to use const, we have to add static
 }
