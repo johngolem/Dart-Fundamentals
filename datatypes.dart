de81ffb536549fd55cdbe5b0d7483963654d9666
John Paul <johnpaul@trees.local>
2022-04-06 07:24:09 +0300

    add data types

---
+++ datatypes.dart
@@ -0,0 +1,25 @@
+// basic data types in dart
+
+// int
+// double
+// string
+// boolean
+// dynamic- can change valueb at runtime
+
+main() {
+  int amount1 = 100;
+
+  var amount2 = 200;
+
+  print('Amount1: $amount1 | Amount2 $amount2 \n');
+
+  String name1 = 'john';
+  var name2 = 'Paul';
+
+  print('My name is $name1 $name2 \n');
+
+  double damount1 = 100.01;
+  var dmount2 = 120.23;
+
+  print('Double1:$damount1 | Double2: $dmount2 \n');
+}
