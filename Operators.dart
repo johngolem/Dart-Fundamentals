e50dfee9d7445b23c63c03fb7067e68ab019510d
John Paul <johnpaul@trees.local>
2022-04-06 08:14:38 +0300

    operators in dart

--- Operators.dart
+++ Operators.dart
@@ -1,3 +1,40 @@
-void main (){
-  
+void main() {
+  int num = 10 + 22;
+  num = num - 2;
+
+  print(num);
+
+  num = num % 5;
+  print(num);
+
+  // Relational operators ==, !=, >=, <=
+
+  if (num == 0) {
+    print('Zero');
+  }
+
+  num = 100;
+  num *= 2;
+
+  /// (shortcut) means output the value of num multiply by 2
+  print("this is the new $num \n");
+
+  // Uranary operators
+  ++num; // pre increment //increase value of num by 1
+  num++; // post increment // increase value of num by 1
+  num += 1;
+
+  /// increase value of num by 1
+  num -= 1; // decrease value of num by 1
+
+  //Logical && and logical ||
+  if (num > 200 && num < 203) {
+    print("200 to 202");
+  }
+
+
+// not equal to
+  if (num != 100) {
+    print("num is not equal to 100");
+  }
 }
